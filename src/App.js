2891c29fa09ebf14b09e655d478585dca1ac9218
Shi Ning Le <shining@ualberta.ca>
2022-12-23 01:59:14 -0700

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
+const Shop = () => {
+    console.log('shop');
+    return <h1>Shop</h1>;
+};
 
 const App = () => {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Home />
+            <Route path="/" element={<Navigation />}>
+                <Route index element={<Home />} />
+                <Route path="shop" element={<Shop />} />
+                <Route path="sign-in" element={<SignIn />} />
+            </Route>
         </Routes>
     );
 };
