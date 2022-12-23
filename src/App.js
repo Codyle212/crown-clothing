caf1b67e72ba82aa56a35ccbd82157c0fa157598
Shi Ning Le <shining@ualberta.ca>
2022-12-22 17:08:19 -0700

    added hardcoded components to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
 const App = () => {
-    return <div className="App">Hello World</div>;
+    const catagories = [
+        {
+            id: 1,
+            title: 'Hats',
+        },
+        {
+            id: 2,
+            title: 'Jackets',
+        },
+        {
+            id: 3,
+            title: 'Sneakers',
+        },
+        {
+            id: 4,
+            title: 'Womens',
+        },
+        {
+            id: 5,
+            title: 'Mens',
+        },
+    ];
+    return (
+        <div className="catagories-container">
+            {catagories.map((catagory) => (
+                <div className="catagory-container">
+                    <div className="background-image">{/* <img /> */}</div>
+                    <div className="catagory-body-container">
+                        <h2>{catagory.title}</h2>
+                        <p>Shop Now</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default App;
