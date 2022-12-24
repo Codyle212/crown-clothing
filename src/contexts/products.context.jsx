2d44e8a37dfce2c375c8c168d653f76896b1e59b
Shi Ning Le <shining@ualberta.ca>
2022-12-23 23:25:50 -0700

    changes product context

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,16 +1,16 @@
 import { createContext, useState } from 'react';
 import PRODUCTS from '../shop-data.json';
 
-export const ProductContext = createContext({
+export const ProductsContext = createContext({
     products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
     const [products, setProducts] = useState(PRODUCTS);
-    const value = products;
+    const value = { products };
     return (
-        <ProductContext.Provider value={value}>
+        <ProductsContext.Provider value={value}>
             {children}
-        </ProductContext.Provider>
+        </ProductsContext.Provider>
     );
 };
